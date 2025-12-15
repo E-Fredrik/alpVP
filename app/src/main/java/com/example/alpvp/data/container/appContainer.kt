6476752b3b1c751f2d1d332a78dbd6feb5b96171
Gee <ssantoso04@student.ciputra.ac.id>
2025-12-15 09:47:37 +0700

    appcontainer
    ww

--- app/src/main/java/com/example/alpvp/data/container/appContainer.kt
+++ app/src/main/java/com/example/alpvp/data/container/appContainer.kt
@@ -1,22 +1,19 @@
 package com.example.alpvp.data.container
 
-import com.example.alpvp.data.repository.AuthRepository
-import com.example.alpvp.data.repository.UserRepository
-import com.example.alpvp.data.repository.FoodRepository
-import com.example.alpvp.data.repository.DashboardRepository
-import com.example.alpvp.data.repository.FoodLogRepository
-import com.example.alpvp.data.repository.FriendRepository
-import com.example.alpvp.data.repository.DailySummaryRepository
+import com.example.alpvp.data.Repository.UserRepository
 import com.example.alpvp.data.Service.FoodService
-import com.example.alpvp.data.Service.UserService
-import com.example.alpvp.data.services.DashboardService
-import com.example.alpvp.data.services.AppService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.getValue
+import kotlin.lazy
+import com.example.alpvp.data.Service.UserService
+import com.example.alpvp.data.Repository.FoodRepository
+
+
 
 class AppContainer {
     companion object {
-        val BASE_URL = "http://10.152.62.60:3000/api/"
+        val BASE_URL = "http://10.0.2.2:3000/api/"
     }
 
     private val retrofit = Retrofit.Builder()
@@ -24,13 +21,12 @@ class AppContainer {
         .baseUrl(BASE_URL)
         .build()
 
-    // Authentication Service (for login/register)
     private val userService: UserService by lazy {
         retrofit.create(UserService::class.java)
     }
 
-    val authRepository: AuthRepository by lazy {
-        AuthRepository(userService)
+    val userRepository: UserRepository by lazy {
+        UserRepository(userService)
     }
 
     private val foodService: FoodService by lazy {
@@ -41,32 +37,4 @@ class AppContainer {
         FoodRepository(foodService)
     }
 
-    private val dashboardService: DashboardService by lazy {
-        retrofit.create(DashboardService::class.java)
-    }
-
-    val dashboardRepository: DashboardRepository by lazy {
-        DashboardRepository(dashboardService)
-    }
-
-    // General App Service (for user profile, friends, food logs, daily summary)
-    private val appService: AppService by lazy {
-        retrofit.create(AppService::class.java)
-    }
-
-    val userRepository: UserRepository by lazy {
-        UserRepository(appService)
-    }
-
-    val foodLogRepository: FoodLogRepository by lazy {
-        FoodLogRepository(appService)
-    }
-
-    val friendRepository: FriendRepository by lazy {
-        FriendRepository(appService)
-    }
-
-    val dailySummaryRepository: DailySummaryRepository by lazy {
-        DailySummaryRepository(appService)
-    }
 }
